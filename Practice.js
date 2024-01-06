efb7de8afcd61a9dbcdc6713c006e2fe8e930c8d
Shoyebaktar shirol <shoyebaktarshirol@gmail.com>
2024-01-06 19:10:57 +0530

    Learnt the arry methods and solved some questions

--- Practice.js
+++ Practice.js
@@ -1,10 +1,4 @@
 
-
-
-
-
-
-
 // let myString = "Hello, World!";
 // console.log(myString);
 
@@ -20,82 +14,77 @@
 
 
 
-// Arithmetic Operators
-let num1 = 10;
-let num2 = 5;
+// Arithmeti  Operators
+// let num1 = 10;
+// let num2 = 5;
 
-console.log("Arithmetic Operators:");
-console.log("Addition:", num1 + num2);
-console.log("Subtraction:", num1 - num2);
-console.log("Multiplication:", num1 * num2);
-console.log("Division:", num1 / num2);
-console.log("Modulus:", num1 % num2);
-console.log("Increment:", ++num1);
-console.log("Decrement:", --num2);
+// console.log("Arithmetic Operators:");
+// console.log("Addition:", num1 + num2);
+// console.log("Subtraction:", num1 - num2);
+// console.log("Multiplication:", num1 * num2);
+// console.log("Division:", num1 / num2);
+// console.log("Modulus:", num1 % num2);
+// console.log("Increment:", ++num1);
+// console.log("Decrement:", --num2);
 
-// Comparison Operators
-let x = 5;
-let y = "5";
+// // Comparison Operato
+// let x = 5;
+// let y = "5";
 
-console.log("\nComparison Operators:");
-console.log("Equal (==):", x == y);
-console.log("Strict Equal (===):", x === y);
-console.log("Not Equal (!=):", x != y);
-console.log("Strict Not Equal (!==):", x !== y);
-console.log("Greater Than (>):", x > num2);
-console.log("Less Than (<):", x < num2);
-console.log("Greater Than or Equal (>=):", x >= num2);
-console.log("Less Than or Equal (<=):", x <= num2);
+// console.log("\nComparison Operators:");
+// console.log("Equal (==):", x == y);
+// console.log("Strict Equal (===):", x === y);
+// console.log("Not Equal (!=):", x != y);
+// console.log("Strict Not Equal (!==):", x !== y);
+// console.log("Greater Than (>):", x > num2);
+// console.log("Less Than (<):", x < num2);
+// console.log("Greater Than or Equal (>=):", x >= num2);
+// console.log("Less Than or Equal (<=):", x <= num2);
 
-// Logical Operators
-let isTrue = true;
-let isFalse = false;
+// // Logical Opeators
+// let isTrue = true;
+// let isFalse = false;
 
-console.log("\nLogical Operators:");
-console.log("AND (&&):", isTrue && isFalse);
-console.log("OR (||):", isTrue || isFalse);
-console.log("NOT (!):", !isTrue);
+// console.log("\nLogical Operators:");
+// console.log("AND (&&):", isTrue && isFalse);
+// console.log("OR (||):", isTrue || isFalse);
+// console.log("NOT (!):", !isTrue);
 
-// Assignment Operators
-let a = 3;
+// // Assignmet Operators
+// let a = 3;
 
-console.log("\nAssignment Operators:");
-a += 2;
-console.log("+=:", a);
-a -= 2;
-console.log("-=:", a);
-a *= 3;
-console.log("*=:", a);
-a /= 2;
-console.log("/=:", a);
-a %= 2;
-console.log("%=:", a);
+// console.log("\nAssignment Operators:");
+// a += 2;
+// console.log("+=:", a);
+// a -= 2;
+// console.log("-=:", a);
+// a *= 3;
+// console.log("*=:", a);
+// a /= 2;
+// console.log("/=:", a);
+// a %= 2;
+// console.log("%=:", a);
 
-// Bitwise Operators
-let num3 = 5;
-let num4 = 3;
+// // Bitwise Operators
+// let num3 = 5;
+// let num4 = 3;
 
-console.log("\nBitwise Operators:");
-console.log("AND (&):", num3 & num4);
-console.log("OR (|):", num3 | num4);
-console.log("XOR (^):", num3 ^ num4);
-console.log("Left Shift (<<):", num3 << 1);
-console.log("Right Shift (>>):", num3 >> 1);
+// console.log("\nBitwise Operators:");
+// console.log("AND (&):", num3 & num4);
+// console.log("OR (|):", num3 | num4);
+// console.log("XOR (^):", num3 ^ num4);
+// console.log("Left Shift (<<):", num3 << 1);
+// console.log("Right Shift (>>):", num3 >> 1);
 
-// Ternary Operator
-let age = 20;
-let eligibility = age >= 18 ? "Eligible" : "Not Eligible";
+// // Ternary Operator
+// let age = 20;
+// let eligibility = age >= 18 ? "Eligible" : "Not Eligible";
 
-console.log("\nTernary Operator:");
-console.log("Eligibility:", eligibility);
-
-
+// console.log("\nTernary Operator:");
+// console.log("Eligibility:", eligibility);
 
 
-
-
-
-// // Conditional statement using if-else
+// // Conditional statemet using if-else
 // let x = 10;
 
 // if (x > 5) {
@@ -103,19 +92,6 @@ console.log("Eligibility:", eligibility);
 // } else {
 //     console.log("x is not greater than 5");
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // // Switch statement for day of the week
@@ -163,8 +139,6 @@ console.log("Eligibility:", eligibility);
 // });
 
 
-
-
 // let items = [250, 645, 300, 900, 50];//length is 5
 // for (let i = 0; i < items.length; i++) {
 //     let offer = items[i] / 10;
@@ -195,12 +169,6 @@ console.log("Eligibility:", eligibility);
 // let result = fruit1.concat(fruit2, fruit3, fruit4);
 
 // console.log(result);
-
-
-
-
-
-
 
 
 // let fruit = ["Potato", "Apple", "Banana", "Kiwi"];
@@ -306,3 +274,65 @@ console.log("Eligibility:", eligibility);
 
 // // Example usage of the countvowls functon with a comment
 // countvowels("I AM SHOYEBAKTAR SHIROL FROM KLE BCA GOKAK"); // Output: 15
+
+
+//for Each loop or Higher order function.forEachloop
+// let arr = ["Mumbai","Hydrabad", "Kerala","Pune", "Bengaluru"];
+// arr.forEach((val, idx, arr) => {
+//     console.log(val.toUpperCase(), idx, arr);
+// });
+
+// let num = [2, 3, 4, 5, 6];
+// num.forEach((num) => {        //forEach accep[t the 3 parameters to perform operations .
+//     // Calculate the square of each number
+//     let result = num * num;
+//     console.log(result);
+// });
+
+//array Methods
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let evenarr= arr.filter((val) => {
+//     return val % 2 == 0; // logic of printing the even nums
+// });
+// console.log(evenarr);
+
+
+//reduce method in js with array,
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const output = arr.reduce((previous, currentValue) => {
+//     return previous + currentValue;
+// }, 0);
+
+// console.log(output);
+
+// let marks = [55, 66, 77, 88, 99];
+// let toppers = marks.filter((val) => {
+//     return val > 90;
+// });
+
+// console.log(toppers);
+
+
+
+
+// Prompt the user for the first nuber
+let num1 = prompt("Enter the first number:");
+
+// Prompt the user for the second number
+let num2 = prompt("Enter the second number:");
+
+// Convert the inpt to numbers
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+// Check if the input is valid or no
+if (!isNaN(num1) && !isNaN(num2)) {
+    // Calculate the sum
+    let sum = num1 + num2;
+
+    // Display the result
+    alert("The sum is: " + sum);
+} else {
+    
+    alert("Invalid input. Please enter valid numbers.");
+}
